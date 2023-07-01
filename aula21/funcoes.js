3991f134fb35be8eac088972cdf4310a9ba209bf
Paulo Trindade <paulotrindade90@outlook.com>
2023-07-01 16:56:30 -0300

    aula dia 01-07

---
+++ aula21/funcoes.js
@@ -0,0 +1,10 @@
+// *********FUNÇÕES*********
+
+// Funções executa ações ou seja mandar um valor pra ela e pegar o valor novamente, mas tem funções que não retorna.
+
+function saudacao(nome) {
+  return `Bom dia ${nome}!`
+}
+
+const variavel = saudacao('Luiz')
+console.log(variavel)
